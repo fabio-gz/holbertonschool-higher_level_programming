e6ede882c50f8f8120fb3ca4f206673dbd4db60d
fabio-gz <fabiogomez.silva@gmail.com>
2019-12-10 17:09:38 +0000

    [Fix] PEP8 style

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,8 +8,10 @@ else:
     last_d = int(math.fmod(number, 10))
 
 if last_d < 6 and last_d != 0:
-    print('Last digit of {:d} is {:d} and is less than 6 and not 0'.format(number, last_d))
+    print('Last digit of {:d} is {:d} and is less than 6 and not 0'.format
+          (number, last_d))
 elif last_d > 5:
-    print('Last digit of {:d} is {:d} and is greater than 5'.format(number, last_d))
+    print('Last digit of {:d} is {:d} and is greater than 5'.format
+          (number, last_d))
 else:
     print('Last digit of {:d} is {:d} and is 0'.format(number, last_d))

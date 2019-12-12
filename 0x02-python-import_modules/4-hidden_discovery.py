0817a5b29ee197e3e933ea3757efc11f5dc390b4
fabio-gz <fabiogomez.silva@gmail.com>
2019-12-12 17:53:11 +0000

    [Fix] main typo

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-from hidden_4 import *
-names = dir()
-for i in names:
-    if i[0] == '_':
-        pass
-    else:
-        print('{}'.format(i))
+if __name__ == "__main__":
+
+    from hidden_4 import *
+    names = dir()
+    for i in names:
+        if i[0] == '_':
+            pass
+        else:
+            print('{}'.format(i))

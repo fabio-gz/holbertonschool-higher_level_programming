1df003ea13f058da3a029a38b56c149ecbc342c0
fabio-gz <fabiogomez.silva@gmail.com>
2019-12-12 17:48:14 +0000

    [Fix] typo

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,9 +9,9 @@ if __name__ == "__main__":
     if lenght == 0:
         print('0 arguments.')
     elif lenght == 1:
-        print('{:d} argument:'.format(lenght))
-        print('{:d}: {:s}'.format(lenght, sys.argv[1]))
+        print('{} argument:'.format(lenght))
+        print('{}: {:s}'.format(lenght, sys.argv[1]))
     elif lenght > 1:
-        print('{:d} arguemnts:'.format(lenght))
+        print('{} arguments:'.format(lenght))
         for i in range(len(argv)):
-            print('{:d}: {:s}'.format(i + 1, sys.argv[i + 1]))
+            print('{}: {:s}'.format(i + 1, sys.argv[i + 1]))

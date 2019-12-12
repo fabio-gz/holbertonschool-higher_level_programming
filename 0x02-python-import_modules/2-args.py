199c8886c70ca15d710076f60bb081bbe71772dc
fabio-gz <fabiogomez.silva@gmail.com>
2019-12-12 16:07:55 +0000

    [Add] arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import sys
+def argv():
+    argv = sys.argv[1:]
+    lenght = len(argv)
+
+    if lenght == 0:
+        print('0 arguments.')
+    elif lenght == 1:
+        print('{:d} argument:'.format(lenght))
+        print('{:d}: {:s}'.format(lenght, sys.argv[1]))
+    elif lenght > 1:
+        print('{:d} arguemnts:'.format(lenght))
+        for i in range(len(argv)):
+            print('{:d}: {:s}'.format(i + 1, sys.argv[i + 1]))
+
+if __name__ == "__main__":
+    argv()

17c3805a543d46cfb2024e34ea02ab9931d65997
fabio-gz <fabiogomez.silva@gmail.com>
2019-12-12 17:37:35 +0000

    [Add] function inside print

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+
     from add_0 import add
 
     a, b = 1, 2
-    c = add(a, b)
-    print('{} + {} = {}'.format(a, b, c))
+    print('{} + {} = {}'.format(a, b, add(a, b)))

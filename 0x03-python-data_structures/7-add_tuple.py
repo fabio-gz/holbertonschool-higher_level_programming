840ec043dbbb162476af89aa0366d0517d45c803
fabio-gz <fabiogomez.silva@gmail.com>
2019-12-18 01:38:00 +0000

    [Add] add tuple

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_a = (tuple_a + (0, 0))
+    tuple_b = (tuple_b + (0, 0))
+    for i, j in zip(tuple_a, tuple_b):
+        res = [i + j, tuple_a[i] + tuple_b[i]]
+        return tuple(res)

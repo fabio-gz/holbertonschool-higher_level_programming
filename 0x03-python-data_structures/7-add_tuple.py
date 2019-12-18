3da048a84e18f96407cecdc9b7ea509d241fbe13
fabio-gz <fabiogomez.silva@gmail.com>
2019-12-18 01:43:46 +0000

    [Fix] use just two elements

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,4 +4,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
     tuple_b = (tuple_b + (0, 0))
     for i, j in zip(tuple_a, tuple_b):
         res = [i + j, tuple_a[i] + tuple_b[i]]
-        return tuple(res)
+        return tuple(res[:2])

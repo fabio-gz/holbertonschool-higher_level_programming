c866ea76083c35dc4ba8b6ca9ae1211243c8678e
fabio-gz <fabiogomez.silva@gmail.com>
2019-12-19 19:27:01 +0000

    [Fix] add value none cond

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,5 +2,8 @@
 def best_score(a_dictionary):
     if a_dictionary:
         return max(a_dictionary)
+        for i in a_dictionary:
+            if a_dictionary[i] is None:
+                return None
     else:
         return None

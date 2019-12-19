4c6d64d919a43e8826a5309cc6eae0de619006a8
fabio-gz <fabiogomez.silva@gmail.com>
2019-12-19 19:42:09 +0000

    [Add] try

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,6 +4,6 @@ def best_score(a_dictionary):
         for i in a_dictionary:
             if a_dictionary[i] is None:
                 return None
-        return max(a_dictionary)
+            return max(a_dictionary)
     else:
         return None

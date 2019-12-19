8006044fb2b03babb8359d50fc1fe018d7adff33
fabio-gz <fabiogomez.silva@gmail.com>
2019-12-19 19:30:20 +0000

    [Fix] rearrange

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        return max(a_dictionary)
         for i in a_dictionary:
             if a_dictionary[i] is None:
                 return None
+        return max(a_dictionary)
     else:
         return None

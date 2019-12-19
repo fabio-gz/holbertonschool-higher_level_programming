41ba0ce6e7035d8c316e8bc19f43019276c1b3fb
fabio-gz <fabiogomez.silva@gmail.com>
2019-12-19 20:39:31 +0000

    [Fix] evaluate values in dict

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        for i in a_dictionary:
-            if a_dictionary[i] is None:
-                return None
-            return max(a_dictionary)
+        return max(a_dictionary, key=a_dictionary.get)
     else:
         return None

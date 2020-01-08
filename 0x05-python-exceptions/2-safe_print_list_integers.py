fa0f051129c0d200d5b9d5e1efbad97d97725147
fabio-gz <fabiogomez.silva@gmail.com>
2020-01-08 20:47:17 +0000

    [Fix] remove isinstance

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     c = 0
-    try:
-        for i in range(x):
-            if isinstance(my_list[i], int):
-                print('{:d}'.format(my_list[i]), end='')
-                c = c + 1
-            else:
-                pass
-        print('')
-        return c
-    except:
-        print('')
-        return c
+    for i in range(x):
+        try:
+            print('{:d}'.format(my_list[i]), end='')
+            c = c + 1
+        except(ValueError, TypeError):
+            pass
+    print('')
+    return c

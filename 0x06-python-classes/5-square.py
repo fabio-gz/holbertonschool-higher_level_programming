020d8fc503542fc78a97010ace44a92d72ef24b8
fabio-gz <fabiogomez.silva@gmail.com>
2020-01-09 20:24:20 +0000

    [Add]print square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+class Square:
+    """class for the area of square"""
+    def __init__(self, size=0):
+        """initial function for square
+        Args:
+             size: size fo square(int)
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """getter func to retieve size
+        Return: returns size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, size=0):
+        """setter func to checl for errors
+        Args:
+            size: square size
+        """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+
+    def area(self):
+        """function to calculate square area
+        Returns:
+            returns the area calcualted
+        """
+        return (self.__size) ** 2
+
+    def my_print(self):
+        """prints to de stdout the character '#'"""
+        if self.__size == 0:
+            print('')
+        else:
+            for i in range(self.__size):
+                print('#' * self.__size)

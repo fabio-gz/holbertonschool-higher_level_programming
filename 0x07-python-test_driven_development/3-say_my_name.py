1f56ecf47bf20e980d7083cebb501c262e80118d
fabio-gz <fabiogomez.silva@gmail.com>
2020-01-13 13:30:33 +0000

    [Add]print my name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+
+Prints first and last name
+
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """function for print names
+    Args:
+        first_name: string for name
+        last_name: string for last name
+    """
+    if not isinstance(first_name, str):
+        raise TypeError('first_name must be a string')
+    if not isinstance(last_name, str):
+        raise TypeError('last_name must be a string')
+
+    print('My name is {} {}'.format(first_name, last_name))

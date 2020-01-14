5e42d5ed437b4c28de091a283fc3b195120f3608
fabio-gz <fabiogomez.silva@gmail.com>
2020-01-14 00:25:06 +0000

    [Add] text indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+
+prints a text with terminations
+
+"""
+
+
+def text_indentation(text):
+    """ function to prints a text a new line every '.', '?' and ':'
+    Args:
+        text: string to print
+    """
+    if not isinstance(text, str):
+        raise TypeError('text must be a string')
+
+    for i in range(len(text)):
+        if i > 0 and (text[i - 1] == '.' or text[i - 1] == '?'\
+                      or text[i - 1] == ':') and text[i] == ' ':
+            continue
+        print('{}'.format(text[i]), end='')
+        if text[i] == '.' or text[i] == '?' or text[i] == ':':
+            print('\n')

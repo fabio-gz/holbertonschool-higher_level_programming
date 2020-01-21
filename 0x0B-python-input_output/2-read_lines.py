bc8c529480d22c09064b0a0828a654772ab1c634
fabio-gz <fabiogomez.silva@gmail.com>
2020-01-21 16:52:28 +0000

    [Add]read lines

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def read_lines(filename="", nb_lines=0):
+    """read a text file and print it
+    Args:
+        filename: text file
+        nb_lines: lines to read
+    """
+    with open('my_file_0.txt', encoding='UTF-8') as f:
+        if nb_lines <= 0:
+            print(f.read(), end='')
+        for i in range(0, nb_lines):
+            print(f.readline(), end='')

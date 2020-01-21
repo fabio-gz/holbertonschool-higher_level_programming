10b6cb6466d138c6d5ab72afacd07dba457c75e6
fabio-gz <fabiogomez.silva@gmail.com>
2020-01-21 13:41:44 +0000

    [fix]typo

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -4,14 +4,14 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     def __init__(self, width, height):
-        self.__width = width
-        self.__height = height
         super().integer_validator('width', width)
         super().integer_validator('height', height)
+        self.__width = width
+        self.__height = height
 
     def area(self):
         return (self.__height * self.__width)
 
     def __str__(self):
         s = '[Rectangle] '
-        return s + '{}/{}'.format(self.__height, self.__width)
+        return s + '{}/{}'.format(self.__width, self.__height)

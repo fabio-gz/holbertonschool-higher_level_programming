174c34a457587f5d82d6457219f012f65ae019ca
fabio-gz <fabiogomez.silva@gmail.com>
2020-01-26 00:44:32 +0000

    [update]json string to file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -10,7 +10,7 @@ class Base:
 
     def __init__(self, id=None):
         """initial function"""
-        if id != None:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
@@ -22,3 +22,17 @@ class Base:
             return '[]'
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes de json representation of lists_objs"""
+        list1 = []
+        filename = cls.__name__ + '.json'
+        with open(filename, mode='w', encoding='UTF-8') as f:
+            if list_objs is None:
+                f.write(str(list1))
+            else:
+                for i in list_objs:
+                    list1.append(cls.to_dictionary(i))
+
+                f.write(cls.to_json_string(list1))

2520a9a5cf7e5147e504b02ac854b6709c716d91
fabio-gz <fabiogomez.silva@gmail.com>
2020-01-27 19:36:28 +0000

    [fix]fix docstring

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-from models.base import Base
 """
 Creation of the rectangle class
 """
+from models.base import Base
 
 
 class Rectangle(Base):
-    """class for Rectangle construction"""
+    """class for Rectangle construction
+    """
     def __init__(self, width, height, x=0, y=0, id=None):
+        """initial function
+        """
         self.width = width
         self.height = height
         self.x = x
@@ -16,12 +19,14 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """retrieve the width"""
+        """retrieve the width
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
-        """setter func for width"""
+        """setter func for width
+        """
         if not isinstance(value, int):
             raise TypeError('width must be an integer')
         elif value <= 0:
@@ -30,12 +35,14 @@ class Rectangle(Base):
 
     @property
     def height(self):
-        """retrieve the height"""
+        """retrieve the height
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
-        """setter func for height"""
+        """setter func for height
+        """
         if not isinstance(value, int):
             raise TypeError('height must be an integer')
         elif value <= 0:
@@ -44,12 +51,14 @@ class Rectangle(Base):
 
     @property
     def x(self):
-        """retrieve the x"""
+        """retrieve the x
+        """
         return self.__x
 
     @x.setter
     def x(self, value):
-        """setter func for x"""
+        """setter func for x
+        """
         if not isinstance(value, int):
             raise TypeError('x must be an integer')
         elif value < 0:
@@ -58,12 +67,14 @@ class Rectangle(Base):
 
     @property
     def y(self):
-        """retrieve the x"""
+        """retrieve the x
+        """
         return self.__y
 
     @y.setter
     def y(self, value):
-        """setter func for x"""
+        """setter func for x
+        """
         if not isinstance(value, int):
             raise TypeError('y must be an integer')
         elif value < 0:
@@ -71,11 +82,13 @@ class Rectangle(Base):
         self.__y = value
 
     def area(self):
-        """returns rectangle area"""
+        """returns rectangle area
+        """
         return self.__width * self.__height
 
     def display(self):
-        """print rectangle taking care of x and y"""
+        """print rectangle taking care of x and y
+        """
         for j in range(self.__y):
             print('')
         for i in range(self.__height):
@@ -84,12 +97,15 @@ class Rectangle(Base):
             print('#' * self.__width)
 
     def __str__(self):
+        """func to print statement
+        """
         return ('[Rectangle] ({}) {}/{} - {}/{}'.format(self.id, self.__x,
                                                         self.__y, self.__width,
                                                         self.__height))
 
     def update(self, *args, **kwargs):
-        """implementation of args and kwargs"""
+        """implementation of args and kwargs
+        """
         attr = ['id', 'width', 'height', 'x', 'y']
         if args is not None and len(args) != 0:
             for i in range(len(args)):
@@ -99,6 +115,7 @@ class Rectangle(Base):
                 setattr(self, key, value)
 
     def to_dictionary(self):
-        """return dict representation of Rectangle"""
+        """return dict representation of Rectangle
+        """
         return {'id': self.id, 'width': self.width, 'height': self.height,
                 'x': self.x, 'y': self.y}
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,33 +1,40 @@
 #!/usr/bin/python3
-from models.rectangle import Rectangle
 """
 Creation of the square class
 """
+from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """class for Square
+    """
     def __init__(self, size, x=0, y=0, id=None):
-        """initial for Square"""
+        """initial for Square
+        """
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
-        """retrieve size"""
+        """retrieve size
+        """
         return self.width
 
     @size.setter
     def size(self, value):
-        """check for size errors"""
+        """check for size errors
+        """
         self.width = value
         self.height = value
 
     def __str__(self):
-        """str for print"""
+        """str for print
+        """
         return ('[Square] ({}) {}/{} - {}'.format(self.id, self.x,
                                                   self.y, self.size))
 
     def update(self, *args, **kwargs):
-        """implementation of args and kwargs"""
+        """implementation of args and kwargs
+        """
         attr = ['id', 'size', 'x', 'y']
         if args is not None and len(args) != 0:
             for i in range(len(args)):
@@ -37,4 +44,6 @@ class Square(Rectangle):
                 setattr(self, key, value)
 
     def to_dictionary(self):
+        """return an attribute dictionary
+        """
         return {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}

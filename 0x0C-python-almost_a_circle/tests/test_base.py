2cbab2b40aad04ad2e6b73d7c5f38bcc24f45f89
fabio-gz <fabiogomez.silva@gmail.com>
2020-01-29 00:30:48 +0000

    [Add]test for pep8

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -3,6 +3,7 @@
 Test for base class
 """
 import unittest
+import pep8
 from models.base import Base
 
 
@@ -32,3 +33,11 @@ class test_base(unittest.TestCase):
         """test for two args"""
         with self.assertRaises(TypeError):
             b1 = Base(2, 3)
+
+    def Testpep8(self):
+        """test for pep8 styleguide"""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/rectangle.py',
+                                        'models/base.py', 'models/square.py'])
+        self.assertEqual(result.total_errors, 0,
+                         'Found code style errors (and warnings).')

10822b2353aa6a81f24d4ea22ca6e566522fde6f
fabio-gz <fabiogomez.silva@gmail.com>
2020-03-02 18:40:54 +0000

    [Add] create, show, drop

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- creates a database
+-- if the database exists the script doesn't fail
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- deletes the database hbtn_0c_0
+-- if doesn't exists it doesn't fail
+DROP DATABASE IF EXISTS hbtn_0c_0;

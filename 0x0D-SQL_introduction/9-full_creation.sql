23e33469aa4465fc8ac4912c806e85f20f4eead0
fabio-gz <fabiogomez.silva@gmail.com>
2020-03-02 19:50:40 +0000

    [Add]new table

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,6 @@
+-- creates a table second_table
+CREATE TABLE IF NOT EXISTS second_table (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256), score INT);
+INSERT INTO second_table (id, name, score) VALUES (NULL, "John", 10);
+INSERT INTO second_table (id, name, score) VALUES (NULL, "Alex", 3);
+INSERT INTO second_table (id, name, score) VALUES (NULL, "Bob", 14);
+INSERT INTO second_table (id, name, score) VALUES (NULL, "George", 8);

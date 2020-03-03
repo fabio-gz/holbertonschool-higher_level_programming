dbcf3f9137efe517af915f5ad03f1f2bb4f0711b
fabio-gz <fabiogomez.silva@gmail.com>
2020-03-03 17:15:25 +0000

    [Add]create user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- creates new user and set password
+-- should not fail if exists
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';

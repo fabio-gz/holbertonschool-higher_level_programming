390a5a63ff00193959a9ecb709b997e85b479365
fabio-gz <fabiogomez.silva@gmail.com>
2020-03-05 03:54:34 +0000

    [fix]typo

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -2,5 +2,5 @@
 -- Each record should display: tv_genres.name - rating sum
 SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating FROM tv_genres JOIN tv_show_genres
        ON tv_show_genres.genre_id = tv_genres.id JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-       JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show:id GROUP BY tv_genres.name
+       JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id GROUP BY tv_genres.name
        ORDER BY rating DESC;

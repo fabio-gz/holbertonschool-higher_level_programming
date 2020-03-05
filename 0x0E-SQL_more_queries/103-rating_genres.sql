124adfd41651cf5134bf09eaba1d9d631de88a06
fabio-gz <fabiogomez.silva@gmail.com>
2020-03-05 03:51:47 +0000

    [Add]best genre

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,6 @@
+-- lists all genres in the database hbtn_0d_tvshows_rate by their rating.
+-- Each record should display: tv_genres.name - rating sum
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating FROM tv_genres JOIN tv_show_genres
+       ON tv_show_genres.genre_id = tv_genres.id JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+       JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show:id GROUP BY tv_genres.name
+       ORDER BY rating DESC;

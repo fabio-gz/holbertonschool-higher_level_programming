7062daa43e296a48b77d220c52ffa6c63ed78bff
fabio-gz <fabiogomez.silva@gmail.com>
2020-03-28 18:49:22 +0000

    [fix]moving out imports

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,10 +2,10 @@
 """
  lists all states with a name starting with N
 """
-if __name__ == '__main__':
-    import MySQLdb
-    import sys
+import MySQLdb
+import sys
 
+if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
 

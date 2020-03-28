9e9a8c4ecbc190cf9c514f9674cff0fab5ab5764
fabio-gz <fabiogomez.silva@gmail.com>
2020-03-28 23:14:16 +0000

    [fix]style

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
 
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states WHERE name= %s COLLATE "
-                   "Latin1_General_cs ORDER BY id ASC",(agmt,))
+                   "Latin1_General_cs ORDER BY id ASC", (agmt,))
     result = cursor.fetchall()
 
     for i in result:

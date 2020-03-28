65b6bda480bd52521c7737167ee634ffd544a1d4
fabio-gz <fabiogomez.silva@gmail.com>
2020-03-28 22:31:07 +0000

    [fix]collate

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,12 +7,11 @@ import sys
 
 if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3],
-                         charset='utf8')
+                         passwd=sys.argv[2], db=sys.argv[3])
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY"
-                   " states.id ASC")
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' "
+                   "COLLATE latin1_General_cs ORDER BY states.id ASC;")
     result = cursor.fetchall()
 
     for i in result:

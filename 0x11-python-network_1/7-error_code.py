66dfc8fd7dee557907897af617d8c96ac9112d42
fabio-gz <fabiogomez.silva@gmail.com>
2020-04-16 18:20:03 +0000

    [Add]error code

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" displays the body of the response."""
+import sys
+import requests
+
+if __name__ = '__main__':
+    r = requests.get(sys.argv[1])
+    if r.status_code >= 400:
+        print("Error code:", r.status_code)
+    else:
+        print(r.text)

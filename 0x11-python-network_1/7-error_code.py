48c41005553f49fdb54d16eb6d66845a1c14309f
fabio-gz <fabiogomez.silva@gmail.com>
2020-04-16 18:21:15 +0000

    [fix]format

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,6 +6,6 @@ import requests
 if __name__ = '__main__':
     r = requests.get(sys.argv[1])
     if r.status_code >= 400:
-        print("Error code:", r.status_code)
+        print("Error code: {}".format(r.status_code))
     else:
         print(r.text)

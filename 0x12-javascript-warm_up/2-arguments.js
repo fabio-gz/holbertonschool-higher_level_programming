0904af393db88566a3c44296e068f0ad13d00ffb
fabio-gz <fabiogomez.silva@gmail.com>
2020-05-05 17:37:32 +0000

    [Add ] conditionals

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const leng = process.argv.length;
+
+if (leng === 2) {
+  console.log('No argument');
+} else if (leng === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}

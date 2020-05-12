dd70db9b7de3891753935b97189e7e843abd0b51
fabio-gz <fabiogomez.silva@gmail.com>
2020-05-12 21:31:42 +0000

    [add]ocurr in a film

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const request = require('request');
+
+const url = process.argv[2];
+const wedge = 'https://swapi-api.hbtn.io/api/people/18/';
+
+const numfilms = function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const dat = JSON.parse(body).results;
+    let num = 0;
+    for (const i of dat) {
+      for (const j of i.characters) {
+        if (j === wedge) {
+          num++;
+        }
+      }
+    }
+    console.log(num);
+  }
+};
+
+request(url, numfilms);
